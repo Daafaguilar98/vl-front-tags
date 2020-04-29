c3a3b0bbb41e7c7d950ed33ae59d3d0e0c823645
Diego Aguilar <daafaguilar98@gmail.com>
2020-04-29 16:27:32 -0500

    add socket io client

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import store from "./store";
+import { connect as connectSocket } from "./plugins/vue-socket-io-extended";
+
+connectSocket(store);
 
 Vue.config.productionTip = false;
 
---
+++ src/plugins/vue-socket-io-extended.ts
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import VueSocketIOExt from 'vue-socket.io-extended';
+import io from 'socket.io-client';
+import { Store, StoreOptions } from "vuex";
+import { State } from "../store/index";
+
+export const connect = (store: Store<State>) => {
+
+    const socket = io(process.env.VUE_SOCKET_SERVER || 'http://localhost:3000');
+    Vue.use(VueSocketIOExt, socket, { store });
+}
