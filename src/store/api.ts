8a6f54588f4b546d617849fcc420dcecc189ddb0
Diego Aguilar <daafaguilar98@gmail.com>
2020-04-30 11:28:24 -0500

    improve Api file

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,30 +1,32 @@
 import axios from "axios";
 
+const VUE_API_REST_URI = process.env.VUE_API_REST_URI || "http://localhost:3000";
+
 export default {
     async getTags() {
         return await axios({
             method: "GET",
-            url: "http://localhost:3000/api/tags"
+            url: `${VUE_API_REST_URI}/api/tags`
           });
     },
     async createTag(data: any) {
         return await axios({
             method: "POST",
-            url: "http://localhost:3000/api/tags",
+            url: `${VUE_API_REST_URI}/api/tags`,
             data
         });
     },
     async updateTag({ id, title }: { id: string, title: string }) {
         return await axios({
             method: "PUT",
-            url: `http://localhost:3000/api/tags/${id}`,
+            url: `${VUE_API_REST_URI}/api/tags/${id}`,
             data: { title }
           });
     },
     async deleteTag(id: string) {
         return await axios({
             method: "DELETE",
-            url: `http://localhost:3000/api/tags/${id}`
+            url: `${VUE_API_REST_URI}/api/tags/${id}`
         });
     }
 }
